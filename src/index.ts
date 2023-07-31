19feaf26e82a75e5ced870d879377598d2e71f76
Prat <chantharamanee.prat@gmail.com>
2023-07-31 17:24:43 +0700

    chore: Impelement FSC class.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-console.log("hello world")
+import {
+  CopyOptions,
+  DeleteOptions,
+  IFileSystemCloud,
+  ListOptions,
+  ReadOptions,
+  WriteOptions,
+} from "./types";
+
+export class FSC implements IFileSystemCloud {
+  private adapter: IFileSystemCloud;
+
+  constructor(adapter: IFileSystemCloud) {
+    this.adapter = adapter;
+  }
+
+  async read(options: ReadOptions): Promise<string | Buffer> {
+    return this.adapter.read(options);
+  }
+
+  async write(options: WriteOptions): Promise<void> {
+    return this.adapter.write(options);
+  }
+
+  async delete(options: DeleteOptions): Promise<void> {
+    return this.adapter.delete(options);
+  }
+
+  async copy(options: CopyOptions): Promise<void> {
+    return this.adapter.copy(options);
+  }
+
+  async list(options: ListOptions): Promise<string[]> {
+    return this.adapter.list(options);
+  }
+}
